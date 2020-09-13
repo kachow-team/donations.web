a11fd54e3a8d94b50717a02ccd847e254cf7a8e3
Maksim Barkalov <maks@fizmatspb.ru>
2020-09-14 01:31:29 +0300

    Улучшил заготовку сниппета

--- mini-app/src/panels/DonationSnippet.js
+++ mini-app/src/panels/DonationSnippet.js
@@ -5,22 +5,17 @@ import Button from '@vkontakte/vkui/dist/components/Button/Button';
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 
-import './CreateHelpHome.css';
-import FormLayout from "@vkontakte/vkui/dist/components/FormLayout/FormLayout";
-import Input from "@vkontakte/vkui/dist/components/Input/Input";
-import Textarea from "@vkontakte/vkui/dist/components/Textarea/Textarea";
-import Checkbox from "@vkontakte/vkui/dist/components/Checkbox/Checkbox";
-import Link from "@vkontakte/vkui/dist/components/Link/Link";
-import FormLayoutGroup from "@vkontakte/vkui/dist/components/FormLayoutGroup/FormLayoutGroup";
-import Select from "@vkontakte/vkui/dist/components/Select/Select";
-import Radio from "@vkontakte/vkui/dist/components/Radio/Radio";
 import PanelHeaderButton from "@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton";
 import {IOS, platform} from "@vkontakte/vkui";
 import Icon28ChevronBack from "@vkontakte/icons/dist/28/chevron_back";
 import Icon24Back from "@vkontakte/icons/dist/24/back";
-import './RegularDonation.css'
 
 import uploadPlaceHolder from '../img/uploadPlaceholder.svg';
+import Header from "@vkontakte/vkui/dist/components/Header/Header";
+import './DonationSnippet.css'
+import Separator from "@vkontakte/vkui/dist/components/Separator/Separator";
+import InfoRow from "@vkontakte/vkui/dist/components/InfoRow/InfoRow";
+import Progress from "@vkontakte/vkui/dist/components/Progress/Progress";
 
 const osName = platform();
 
@@ -33,17 +28,33 @@ class DonationSnippet extends React.Component {
                         {osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
                     </PanelHeaderButton>}
                 >
-                   Новости
+                    Новости
                 </PanelHeader>
                 <Div>
                     <div style={{
                         backgroundImage: `url(${this.props.donation.imagePreviewUrl === '' ? uploadPlaceHolder : this.props.donation.imagePreviewUrl})`,
                         //width:"375px",
-                        height:"153px",
-                        borderRadius:"10px",
+                        height: "153px",
+                        borderTopLeftRadius: "10px",
+                        borderTopRightRadius: "10px",
                         backgroundSize: "cover",
                         backgroundPosition: "center top"
-                    }} />
+                    }}/>
+
+                    <Group header={
+                        <Header subtitle="Максим Баркалов">
+                            Другие сервисы VK
+                        </Header>
+                    }>
+                        <Separator/>
+                        <Div className={'Sum'}>
+                            <InfoRow header="Собрано в сентябре 8500₽">
+                                <Progress value={40}/>
+                            </InfoRow>
+                                <Button mode="outline">Помочь</Button>
+                        </Div>
+                    </Group>
+
                 </Div>
             </Panel>
         );
--- mini-app/src/panels/RegularDonation.js
+++ mini-app/src/panels/RegularDonation.js
@@ -2,18 +2,11 @@ import React from 'react';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
-import Group from '@vkontakte/vkui/dist/components/Group/Group';
-import Div from '@vkontakte/vkui/dist/components/Div/Div';
 
-import './CreateHelpHome.css';
 import FormLayout from "@vkontakte/vkui/dist/components/FormLayout/FormLayout";
 import Input from "@vkontakte/vkui/dist/components/Input/Input";
 import Textarea from "@vkontakte/vkui/dist/components/Textarea/Textarea";
-import Checkbox from "@vkontakte/vkui/dist/components/Checkbox/Checkbox";
-import Link from "@vkontakte/vkui/dist/components/Link/Link";
-import FormLayoutGroup from "@vkontakte/vkui/dist/components/FormLayoutGroup/FormLayoutGroup";
 import Select from "@vkontakte/vkui/dist/components/Select/Select";
-import Radio from "@vkontakte/vkui/dist/components/Radio/Radio";
 import PanelHeaderButton from "@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton";
 import {IOS, platform} from "@vkontakte/vkui";
 import Icon28ChevronBack from "@vkontakte/icons/dist/28/chevron_back";
@@ -67,7 +60,7 @@ class RegularDonation extends React.Component {
                     <FormLayout>
                         <div onClick={() => this.triggerClick()} style={{
                             backgroundImage: `url(${this.state.imagePreviewUrl === '' ? uploadPlaceHolder : this.state.imagePreviewUrl})`,
-                            width:"375px",
+                            //width:"375px",
                             height:"153px",
                             borderRadius:"10px",
                             backgroundSize: "cover",
